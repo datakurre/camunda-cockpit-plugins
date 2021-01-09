59fd2b33b8cf8034f576b52a7d15f950c5455169
Asko Soukka <asko.soukka@iki.fi>
2021-01-09 19:53:53 +0200

    Re-enable sequence rendering

--- src/Components/BPMN.tsx
+++ src/Components/BPMN.tsx
@@ -5,6 +5,7 @@ import tooltips from 'diagram-js/lib/features/tooltips';
 import React, { useEffect, useRef } from 'react';
 
 import RobotModule from '../RobotModule';
+import { renderSequenceFlow } from '../utils/bpmn';
 
 export const BPMNViewer = async (diagram: string) => {
   const model = new BpmnViewer({
@@ -75,6 +76,7 @@ const BPMN: React.FC<Props> = ({ activities, className, diagramXML, style }) => 
         const canvas = viewer.get('canvas');
         canvas.zoom('fit-viewport');
         renderActivities(viewer, activities ?? []);
+        renderSequenceFlow(viewer, activities ?? []);
       }
     })();
   }, [diagramXML]);
