57a7988a4525e15bf6589dfb4366377b147a1b52
Asko Soukka <asko.soukka@iki.fi>
2021-04-10 14:31:20 +0300

    Enable sort on audit log table

--- src/Components/AuditLogTable.tsx
+++ src/Components/AuditLogTable.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useTable } from 'react-table';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
+import { TiMinus } from 'react-icons/ti';
+import { useSortBy, useTable } from 'react-table';
 
 import { asctime } from '../utils/misc';
 
@@ -69,7 +71,7 @@ const AuditLogTable: React.FC<Props> = ({ activities, decisions }) => {
       }),
     [activities, decisions]
   );
-  const tableInstance = useTable({ columns: columns as any, data });
+  const tableInstance = useTable({ columns: columns as any, data }, useSortBy);
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
   return (
     <table className="cam-table" {...getTableProps()}>
@@ -77,7 +79,25 @@ const AuditLogTable: React.FC<Props> = ({ activities, decisions }) => {
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              /* @ts-ignore */
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render('Header')}
+                <span style={{ position: 'absolute', fontSize: '125%' }}>
+                  {
+                    /* @ts-ignore */
+                    column.isSorted ? (
+                      /* @ts-ignore */
+                      column.isSortedDesc ? (
+                        <GoChevronDown style={{ color: '#155cb5' }} />
+                      ) : (
+                        <GoChevronUp style={{ color: '#155cb5' }} />
+                      )
+                    ) : (
+                      <TiMinus style={{ color: '#155cb5' }} />
+                    )
+                  }
+                </span>
+              </th>
             ))}
           </tr>
         ))}
