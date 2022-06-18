2fa53145616cb4302170bcf23acb8b24f266dab2
Asko Soukka <asko.soukka@iki.fi>
2022-06-18 12:20:46 +0300

    Remove tasklist audit log plugin from example config.js, because it is not a cockpit plugin

--- config.js
+++ config.js
@@ -27,8 +27,7 @@ export default {
   //   // 'my-custom-script/customScript'
           'scripts/definition-historic-activities.js',
           'scripts/instance-historic-activities.js',
-          'scripts/instance-route-history.js',
-          'scripts/tasklist-audit-log.js'
+          'scripts/instance-route-history.js'
        // 'scripts/instance-tab-modify.js'
      ],
   // requireJsConfig: {
